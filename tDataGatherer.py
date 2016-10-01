34d6490ab639f54ab1107704dd4274ea339eb5c0
Brian Walker <Brian Walker>
2016-10-01 18:20:36 -0500

    Remove old code

--- tDataGatherer.py
+++ tDataGatherer.py
@@ -9,6 +9,8 @@ api = twitter.Api(consumer_key= keyData.consumer_key,
                   access_token_secret= keyData.access_token_secret)
 #NOTE ONLY 100 API CALLS PER HOUR
 
+#SAMPLE CODE
+#this would print out all of the people I follow
 # users = api.GetFriends()
 # for user in users:
 # 	print 'Name: ' + str(user.screen_name)
@@ -17,17 +19,14 @@ api = twitter.Api(consumer_key= keyData.consumer_key,
 # 	print 
 
 statuses = api.GetUserTimeline(screen_name = 'bencgauld')
-timeOfTweets = defaultdict(lambda:defaultdict(lambda:0))
-timesList = []
-#combinedStatus = ''
-for status in statuses:
-	timesList.append(status.created_at)
-	print status.created_at
-	year = status.created_at[26:30]
-	day = status.created_at[0:3]
-	hour = status.created_at[11:13]
-	#combinedStatus += ' ' + status.text   UNICODE ENCODING ERROR
-	timeOfTweets[year][(day,hour)] += 1
 
-print timeOfTweets
-#print combinedStatus
+def getTweetTimesByYear(statuses):
+	timeOfTweets = defaultdict(lambda:defaultdict(lambda:0))
+	for status in statuses:
+		year = status.created_at[26:30]
+		day = status.created_at[0:3]
+		hour = status.created_at[11:13]
+		timeOfTweets[year][(day,hour)] += 1
+	return timeOfTweets
+
+print getTweetTimesByYear(statuses)
