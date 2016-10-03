317d7aa0b4aaa82661bf70c2de4d66b4c45e60ad
Brian Walker <Brian Walker>
2016-10-03 17:23:49 -0500

    Added bar graph for time of tweets

--- DataProcesser.py
+++ DataProcesser.py
@@ -1,22 +1,47 @@
 import tDataGatherer
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 t = tDataGatherer.TDataGatherer()
-t.fetchStatuses('bencgauld', 3200)
+t.fetchStatuses('bencgauld', 1000)
 statuses = t.getFullStatuses()
 tweets = t.getTweets()
-print tweets
 times = t.getTimes()
 
-def printTweetTimesByDayTime(times):
+def plotTweetTimesByDayTime(times):
 	timeOfTweets = defaultdict(lambda:0)
 	for time in times:
 		timeOfTweets[(time[1],time[2])] += 1
 	days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 	times = range(24)
-	for time in times:
-		for day in days:
-			print str(day) + " " + str(time) + " :" + str(timeOfTweets[(day,time)]),
-		print
+	dayTimes = []
+	numbers = []
+	for day in days:
+		for time in times:
+			dayTimes.append((day,time))
+			numbers.append(timeOfTweets[(day,time)])
+	fig, ax = plt.subplots()
+	width = 1
+	rects1 = ax.bar(range(168),numbers, width)
+	ax.set_xticklabels(days)
+	ax.set_xticks((0, 24, 48, 72, 96, 120, 144))
+	plt.show()
 
-printTweetTimesByDayTime(times)
+def plotTweetTimesByTime(times):
+	timeOfTweets = defaultdict(lambda:0)
+	for time in times:
+		timeOfTweets[time[2]] += 1
+	times = range(24)
+	numbers = []
+	for time in times:
+		numbers.append(timeOfTweets[time])
+	fig, ax = plt.subplots()
+	width = 1
+	rects1 = ax.bar(times,numbers, width)
+	ax.set_xticklabels([0, 3, 6, 9, 12, 3, 6, 9])
+	ax.set_xticks((0, 3, 6, 9, 12, 15, 18, 21))
+	plt.show()
+
+
+plotTweetTimesByDayTime(times)
+plotTweetTimesByTime(times)
--- tDataGatherer.py
+++ tDataGatherer.py
@@ -13,6 +13,7 @@ class TDataGatherer:
 
 	def fetchStatuses(self, username, maxCount): #maxCount max is 3200
 		#Just get statuses here, don't process at all
+		self.user = self.api.GetUser(screen_name = username)
 		batchCount = min(200, maxCount) #200 fetch limit
 		self.statuses = self.api.GetUserTimeline(screen_name = username, count = batchCount)
 		fetchCount = batchCount
@@ -30,12 +31,15 @@ class TDataGatherer:
 		#only the tweets themselves
 		tweets = []
 		for status in self.statuses:
-			tweets.append(str(status.text))
+			tweets.append(status.text)#Unicode / ascii errors when looking at strings
 		return tweets
 
 	def getTimes(self):
 		#only the times themselves
+		#issue-- accounting for timezone - user is supposed to have utc offset but doesn't...
 		timeOfTweets = []
+		previousDay = {"Sun":"Sat", "Mon":"Sun", "Tue":"Mon", "Wed":"Tue", "Thu":"Wed", "Fri":"Thu", "Sat":"Fri"}
+		nextDay = {"Sun":"Mon", "Mon":"Tue", "Tue":"Wed", "Wed":"Thu", "Thu":"Fri", "Fri":"Sat", "Sat":"Sun"}
 		for status in self.statuses:
 			year = int(status.created_at[26:30])
 			day = status.created_at[0:3]
