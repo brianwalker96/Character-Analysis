c7ca4dd6851f9b9bc99f0637162af8c92e3f3c10
Brian Walker <Brian Walker>
2016-12-06 17:56:25 -0600

    shhh

---
+++ dAccessToken.py
@@ -0,0 +1,5 @@
+class dAccessToken:
+	def __init__(self):
+		self.user_id = '8869'
+		self.api_key = 'b313da6437a6a9632d18f4adf9549196'
+		self.subscription_id = '8834'
---
+++ tAccessToken.py
@@ -0,0 +1,7 @@
+class AccessToken:
+	def __init__(self):
+		self.consumer_key= 'A9zsdUlncih3YKcY6W9KXGzwQ'
+		self.consumer_secret = 'Nw4OTRscPyYWdOJjBCNNIo0kJWgbF1wA5VsaFHnm3uA3hzGMju'
+		self.access_token_key='402525444-IdkWxCkIsX09a9SACicKX0x68PltTXRzqKFqWmNA'
+		self.access_token_secret='u2cuNAq0pX3W8rSa3FlUc4qBYjhrdqt7nEScVyHe1wi3a'
+		
